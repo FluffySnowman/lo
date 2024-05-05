6f74407c9a25b5262950f096568b72bbaf7614ba
fluffy <fluffy@fluffysnowman.dev>
2024-05-05 14:15:48 +0530

    added total size display

--- src/main.go
+++ src/main.go
@@ -143,7 +143,13 @@ func main() {
 		return files[i].ModTime().After(files[j].ModTime())
 	})
 
-	fmt.Printf("\nCWD: %s\tTotal Filez %s\n\n", color.New(color.FgHiMagenta).Sprint(dirPath), color.New(color.FgHiCyan, color.Italic, color.Bold).Sprintf("%d", len(files)))
+	var totalSize int64
+	for _, file := range files {
+		totalSize += file.Size()
+	}
+
+	fmt.Printf("\nCWD: %s\tTotal Files %s\n", color.New(color.FgHiMagenta).Sprint(dirPath), color.New(color.FgHiCyan, color.Italic, color.Bold).Sprintf("%d", len(files)))
+	fmt.Printf("Total Size: %s\n\n", formatSize(totalSize))
 	fmt.Printf("%-4s\t %-10s\t %-25s%s\n", "ID", "Size", "Modified", "File")
 	for i, file := range files {
 		filename := printColoredName(file)
